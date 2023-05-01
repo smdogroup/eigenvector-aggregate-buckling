9778ff8cfa4aed2ba769548513187515c0761ba1
Bao Li <libao@gatech.edu>
2023-05-01 12:09:31 -0400

    update run script

--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ if [ ! -d output ]; then
 fi
 
 ################# simple example #################
-python3.10 simple_example.py
+python3 simple_example.py
 
 ###################### demo ######################
-python3.10 demo.py
+python3 demo.py
 
 ###################### tube ######################
-python3.10 tube_opt.py
+python3 tube_opt.py
 
 ###################### beam ######################
-python3.10 topo_opt.py --domain beam --objf frequency --confs volume --nx 200 --r0 1.0 --vol-frac-ub 0.5 --maxit 100 
+python3 topo_opt.py --domain beam --objf frequency --confs volume --nx 200 --r0 1.0 --vol-frac-ub 0.5 --maxit 100 
 
 ###################### square ######################
-python3.10 topo_opt.py --domain square --objf frequency --confs volume --nx 100 --r0 1.0 --vol-frac-ub 0.4 --maxit 100
+python3 topo_opt.py --domain square --objf frequency --confs volume --nx 100 --r0 1.0 --vol-frac-ub 0.4 --maxit 100
