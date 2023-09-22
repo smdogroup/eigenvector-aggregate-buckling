2988236f7bc29c9375b4f22c7e8604b3888636a2
Bao Li <libao@gatech.edu>
2023-09-22 16:29:01 -0400

    K and G can dealing with complex data type

--- src/warpper.cpp
+++ src/warpper.cpp
@@ -1,4 +1,5 @@
 // #include <omp.h>
+#include <pybind11/complex.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -25,6 +26,22 @@ py::array_t<D> assembleK(py::array_t<D> rho_py, py::array_t<double> X_py, py::ar
   View3D<D> Ke = computeK<double, D>(X, conn, rho, C0, rho0_K, ptype_K, p, q);
 
   return viewToNumpyArray3D<D>(Ke);
+}
+
+// Wrapper function that dispatches to the appropriate add function
+// based on the data types for rho_py
+py::object assembleK_generic(py::object rho_py, py::array_t<double> X_py, py::array_t<int> conn_py,
+                             py::array_t<double> C0_py, double rho0_K, const char* ptype_K,
+                             double p, double q) {
+  if (py::isinstance<py::array_t<double>>(rho_py)) {
+    return assembleK<double>(rho_py.cast<py::array_t<double>>(), X_py, conn_py, C0_py, rho0_K,
+                             ptype_K, p, q);
+  } else if (py::isinstance<py::array_t<std::complex<double>>>(rho_py)) {
+    return assembleK<std::complex<double>>(rho_py.cast<py::array_t<std::complex<double>>>(), X_py,
+                                           conn_py, C0_py, rho0_K, ptype_K, p, q);
+  } else {
+    throw std::runtime_error("Unsupported data type for rho_py");
+  }
 }
 
 template <typename T>
@@ -62,6 +79,23 @@ py::array_t<D> assembleG(py::array_t<double> X_py, py::array_t<int> conn_py, py:
   return viewToNumpyArray3D<D>(Ge);
 }
 
+// Wrapper function that dispatches to the appropriate add function
+// based on the data types for rho_py
+py::object assembleG_generic(py::object rho_py, py::object u_py, py::array_t<double> X_py,
+                             py::array_t<int> conn_py, py::array_t<double> C0_py, double rho0_K,
+                             const char* ptype_K, double p, double q) {
+  if (py::isinstance<py::array_t<double>>(rho_py)) {
+    return assembleG<double>(X_py, conn_py, rho_py.cast<py::array_t<double>>(),
+                             u_py.cast<py::array_t<double>>(), C0_py, rho0_K, ptype_K, p, q);
+  } else if (py::isinstance<py::array_t<std::complex<double>>>(rho_py)) {
+    return assembleG<std::complex<double>>(
+        X_py, conn_py, rho_py.cast<py::array_t<std::complex<double>>>(),
+        u_py.cast<py::array_t<std::complex<double>>>(), C0_py, rho0_K, ptype_K, p, q);
+  } else {
+    throw std::runtime_error("Unsupported data type for rho_py");
+  }
+}
+
 template <typename T>
 std::tuple<py::array_t<T>, py::array_t<T>, py::array_t<T>> assembleGDerivative(
     py::array_t<T> X_py, py::array_t<int> conn_py, py::array_t<T> rho_py, py::array_t<T> u_py,
@@ -89,15 +123,9 @@ std::tuple<py::array_t<T>, py::array_t<T>, py::array_t<T>> assembleGDerivative(
 }
 
 PYBIND11_MODULE(kokkos, m) {
-  m.def("assemble_stiffness_matrix", &assembleK<double>, "Assemble the stiffness matrix");
+  m.def("assemble_stiffness_matrix", &assembleK_generic, "Assemble the stiffness matrix");
 
-  m.def("assemble_stiffness_matrix_complex", &assembleK<std::complex<double>>,
-        "Assemble the stiffness matrix");
-
-  m.def("assemble_stress_stiffness", &assembleG<double>, "Assemble the stress stiffness matrix");
-
-  m.def("assemble_stress_stiffness_complex", &assembleG<std::complex<double>>,
-        "Assemble the stress stiffness matrix");
+  m.def("assemble_stress_stiffness", &assembleG_generic, "Assemble the stress stiffness matrix");
 
   m.def("stiffness_matrix_derivative", &assembleKDerivative<double>,
         "Compute the derivative of the stiffness matrix");
@@ -108,41 +136,41 @@ PYBIND11_MODULE(kokkos, m) {
   m.def("initialize_kokkos", &initializeKokkos, "Initialize Kokkos");
 
   m.def("finalize_kokkos", &finalizeKokkos, "Finalize Kokkos");
-
-  // py::class_<Buckling<double>>(m, "Buckling")
-  //     .def(py::init(
-  //         [](py::array_t<double> X_py, py::array_t<int> conn_py, py::array_t<double> rho_py,
-  //            py::array_t<double> u_py, py::array_t<double> psi_py, py::array_t<double> phi_py,
-  //            py::array_t<double> C0_py, double rho0_K, std::string ptype_K, double p, double q) {
-  //           // Convert numpy arrays to Kokkos views
-  //           auto X = numpyArrayToView2D<double>(X_py);
-  //           auto conn = numpyArrayToView2D<int>(conn_py);
-  //           auto rho = numpyArrayToView1D<double>(rho_py);
-  //           auto u = numpyArrayToView1D<double>(u_py);
-  //           auto psi = numpyArrayToView1D<double>(psi_py);
-  //           auto phi = numpyArrayToView1D<double>(phi_py);
-  //           auto C0 = numpyArrayToView2D<double>(C0_py);
-
-  //           // Create the Buckling object
-  //           return new Buckling<double>(X, conn, rho, u, psi, phi, C0, rho0_K, ptype_K, p, q);
-  //         }))
-  //     .def("computeK",
-  //          [](Buckling<double>& self) {
-  //            View3D<double> K = self.computeK();
-  //            return viewToNumpyArray3D(K);
-  //          })
-  //     .def("computeG",
-  //          [](Buckling<double>& self) {
-  //            View3D<double> G = self.computeG();
-  //            return viewToNumpyArray3D(G);
-  //          })
-  //     .def("computeKDerivative",
-  //          [](Buckling<double>& self) {
-  //            View1D<double> dK = self.computeKDerivative();
-  //            return viewToNumpyArray1D(dK);
-  //          })
-  //     .def("computeGDerivative", [](Buckling<double>& self) {
-  //       View1D<double> dG = self.computeGDerivative();
-  //       return viewToNumpyArray1D(dG);
-  //     });
 }
+
+// py::class_<Buckling<double>>(m, "Buckling")
+//     .def(py::init(
+//         [](py::array_t<double> X_py, py::array_t<int> conn_py, py::array_t<double> rho_py,
+//            py::array_t<double> u_py, py::array_t<double> psi_py, py::array_t<double> phi_py,
+//            py::array_t<double> C0_py, double rho0_K, std::string ptype_K, double p, double q) {
+//           // Convert numpy arrays to Kokkos views
+//           auto X = numpyArrayToView2D<double>(X_py);
+//           auto conn = numpyArrayToView2D<int>(conn_py);
+//           auto rho = numpyArrayToView1D<double>(rho_py);
+//           auto u = numpyArrayToView1D<double>(u_py);
+//           auto psi = numpyArrayToView1D<double>(psi_py);
+//           auto phi = numpyArrayToView1D<double>(phi_py);
+//           auto C0 = numpyArrayToView2D<double>(C0_py);
+
+//           // Create the Buckling object
+//           return new Buckling<double>(X, conn, rho, u, psi, phi, C0, rho0_K, ptype_K, p, q);
+//         }))
+//     .def("computeK",
+//          [](Buckling<double>& self) {
+//            View3D<double> K = self.computeK();
+//            return viewToNumpyArray3D(K);
+//          })
+//     .def("computeG",
+//          [](Buckling<double>& self) {
+//            View3D<double> G = self.computeG();
+//            return viewToNumpyArray3D(G);
+//          })
+//     .def("computeKDerivative",
+//          [](Buckling<double>& self) {
+//            View1D<double> dK = self.computeKDerivative();
+//            return viewToNumpyArray1D(dK);
+//          })
+//     .def("computeGDerivative", [](Buckling<double>& self) {
+//       View1D<double> dG = self.computeGDerivative();
+//       return viewToNumpyArray1D(dG);
+//     });
