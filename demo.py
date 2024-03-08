4492f5d90aa44065a232085b0d4826de36aab270
Bao Li <libao@gatech.edu>
2024-03-08 12:33:50 -0500

    clean code

--- demo.py
+++ demo.py
@@ -1,14 +1,12 @@
 from icecream import ic
 import matplotlib.pylab as plt
-from mpl_toolkits.mplot3d import Axes3D, proj3d
 import mpmath as mp
 import numpy as np
-from scipy.linalg import eig, eigh, expm, eigvalsh
-from scipy.optimize import minimize
+from scipy.linalg import eigh, eigvalsh, expm
 import scipy.sparse
 
 
-def rand_symm_mat(n=10, eig_low=0.1, eig_high=100.0, nrepeat=1):
+def rand_symm_mat(n=10, eig_low=0.1, eig_high=100.0, nrepeat=3):
     # Randomly generated matrix that will be used to generate the eigenvectors
     QRmat = -1.0 + 2 * np.random.uniform(size=(n, n))
 
@@ -130,10 +128,13 @@ def Eij_a(fun, rho, trace, lam_min, lam1, lam2):
     return np.float64(val)
 
 
-def Gij_a(fun, rho, trace, lam_min, lam1, lam2):
+def Fij_a(fun, rho, trace, lam_min, lam1, lam2):
     with mp.workdps(80):
         if lam1 == lam2:
-            val = -rho * lam1 * fun(-rho * (lam1 - lam_min)) / trace
+            val = (
+                -rho * lam1 * fun(-rho * (lam1 - lam_min)) / trace
+                + fun(-rho * (lam1 - lam_min)) / trace
+            )
         else:
             val = (
                 lam1 * fun(-rho * (lam1 - lam_min))
@@ -154,13 +155,12 @@ def Eij_ab(fun, rho, trace, lam1, lam2, lam_a, lam_b):
 
         if lam1 == lam2:
             val = -rho * eta1 * (a1 + b1) / mp.mpf(trace)
-            # val = 0.0
         else:
             val = (eta1 - eta2) / (mp.mpf(lam1) - mp.mpf(lam2)) / mp.mpf(trace)
     return np.float64(val)
 
 
-def Gij_ab(fun, rho, trace, lam1, lam2, lam_a, lam_b):
+def Fij_ab(fun, rho, trace, lam1, lam2, lam_a, lam_b):
     with mp.workdps(80):
         a1 = fun(rho * (mp.mpf(lam1) - mp.mpf(lam_a)))
         b1 = fun(rho * (mp.mpf(lam1) - mp.mpf(lam_b)))
@@ -171,8 +171,7 @@ def Gij_ab(fun, rho, trace, lam1, lam2, lam_a, lam_b):
         eta2 = a2 - b2
 
         if lam1 == lam2:
-            val = -rho * lam1 * eta1 * (a1 + b1) / mp.mpf(trace)
-            # val = 0.0
+            val = -rho * lam2 * eta2 * (a2 + b2) / mp.mpf(trace) + eta1 / mp.mpf(trace)
         else:
             val = (
                 (lam1 * eta1 - lam2 * eta2)
@@ -182,10 +181,9 @@ def Gij_ab(fun, rho, trace, lam1, lam2, lam_a, lam_b):
     return np.float64(val)
 
 
-def deriv_exict(
+def deriv_exict_EF(
     rho,
     A,
-    B,
     D,
     Adot,
     Bdot,
@@ -194,7 +192,7 @@ def deriv_exict(
     eta,
     fun,
     Eij_fun,
-    Gij_fun,
+    Fij_fun,
     lam_a=None,
     lam_b=None,
     Ddot=None,
@@ -205,8 +203,6 @@ def deriv_exict(
 
     trace = np.sum(eta)
     eta = eta / trace
-
-    # compute the h value
     h = fun_h(eta, Q, D)
     hdot = 0.0
 
@@ -218,20 +214,20 @@ def deriv_exict(
 
             if Eij_fun == Eij_a:
                 Eij = Eij_fun(fun, rho, trace, np.min(lam), lam[i], lam[j])
-                Gij = Gij_fun(fun, rho, trace, np.min(lam), lam[i], lam[j])
+                Fij = Fij_fun(fun, rho, trace, np.min(lam), lam[i], lam[j])
             else:
                 Eij = Eij_fun(fun, rho, trace, lam[i], lam[j], lam_a, lam_b)
-                Gij = Gij_fun(fun, rho, trace, lam[i], lam[j], lam_a, lam_b)
+                Fij = Fij_fun(fun, rho, trace, lam[i], lam[j], lam_a, lam_b)
 
             if i == j:
                 scale = qDq - h
             else:
                 scale = qDq
 
-            hdot += scale * (Eij * Adot_q - Gij * Bdot_q)
+            hdot += scale * (Eij * Adot_q - Fij * Bdot_q)
 
     for j in range(np.shape(A)[0]):
-        hdot -= eta[j] * (Q[:, j].T @ D @ Q[:, j]) * (Q[:, j].T @ Bdot @ Q[:, j])
+        hdot -= eta[j] * h * (Q[:, j].T @ Bdot @ Q[:, j])
 
         if Ddot is not None:
             hdot += eta[j] * Q[:, j].T @ Ddot @ Q[:, j]
@@ -239,7 +235,56 @@ def deriv_exict(
     return hdot
 
 
-def deriv_exict2(rho, A, B, D, Adot, Bdot, Ddot=None):
+def deriv_exict_E(
+    rho,
+    A,
+    D,
+    Adot,
+    Bdot,
+    lam,
+    Q,
+    eta,
+    fun,
+    Eij_fun,
+    lam_a=None,
+    lam_b=None,
+    Ddot=None,
+):
+    """
+    Compute the forward mode derivative
+    """
+
+    trace = np.sum(eta)
+    eta = eta / trace
+    h = fun_h(eta, Q, D)
+    hdot = 0.0
+
+    for j in range(np.shape(A)[0]):
+        for i in range(np.shape(A)[0]):
+            Adot_q = Q[:, i].T @ Adot @ Q[:, j]
+            Bdot_q = Q[:, i].T @ Bdot @ Q[:, j]
+            Dq = Q[:, i].T @ D @ Q[:, j]
+
+            if Eij_fun == Eij_a:
+                Eij = Eij_fun(fun, rho, trace, np.min(lam), lam[i], lam[j])
+            else:
+                Eij = Eij_fun(fun, rho, trace, lam[i], lam[j], lam_a, lam_b)
+
+            if i == j:
+                hdot += Eij * (Dq - h) * (Adot_q - lam[j] * Bdot_q)
+            else:
+                hdot += Eij * Dq * (Adot_q - lam[j] * Bdot_q)
+
+            hdot -= eta[i] * Dq * Bdot_q
+
+    for j in range(np.shape(A)[0]):
+        if Ddot is not None:
+            hdot += eta[j] * Q[:, j].T @ Ddot @ Q[:, j]
+
+    return hdot
+
+
+def deriv_exict3(rho, A, B, D, Adot, Bdot, Ddot=None):
     lam, Q = eigh(A, B)
 
     lam_min = np.min(lam)
@@ -284,7 +329,97 @@ def deriv_exict2(rho, A, B, D, Adot, Bdot, Ddot=None):
     return hdot
 
 
-def deriv_approx(
+def deriv_approx_EF(
+    A,
+    B,
+    D,
+    Adot,
+    Bdot,
+    lam,
+    Q,
+    eta,
+    fun,
+    Eij_fun,
+    Fij_fun,
+    N_a=0,
+    N_b=5,
+    lam_a=None,
+    lam_b=None,
+    Ddot=None,
+    rho=1.0,
+):
+    """
+    Eignvector Aggregation II
+    """
+    # normalize the eta vector
+    trace = np.sum(eta)
+    eta = eta / trace
+    h = fun_h(eta, Q, D)
+    hdot = 0.0
+
+    for i in range(N_a, N_b):
+        for j in range(i + 1):
+            qDq = Q[:, i].T @ D @ Q[:, j]
+            qAdotq = Q[:, i].T @ Adot @ Q[:, j]
+            qBdotq = Q[:, i].T @ Bdot @ Q[:, j]
+
+            if Eij_fun == Eij_a:
+                Eij = Eij_fun(fun, rho, trace, np.min(lam), lam[i], lam[j])
+                Fij = Fij_fun(fun, rho, trace, np.min(lam), lam[i], lam[j])
+            else:
+                Eij = Eij_fun(fun, rho, trace, lam[i], lam[j], lam_a, lam_b)
+                Fij = Fij_fun(fun, rho, trace, lam[i], lam[j], lam_a, lam_b)
+
+            if i == j:
+                scale = qDq - h
+            else:
+                scale = 2 * qDq
+
+            hdot += scale * (Eij * qAdotq - Fij * qBdotq)
+
+    # compute the orthogonal projector
+    C = B @ Q[:, :N_b]
+    U, _ = np.linalg.qr(C)
+
+    factor = 0.99
+    P = A - factor * lam[0] * B
+    P = scipy.sparse.csc_matrix(P)
+    Pfactor = scipy.sparse.linalg.factorized(P)
+
+    def preconditioner(x):
+        y = Pfactor(x)
+        t = np.dot(U.T, y)
+        y = y - np.dot(U, t)
+        return y
+
+    preop = scipy.sparse.linalg.LinearOperator((n, n), matvec=preconditioner)
+
+    for j in range(np.max([0, N_a - 1]), N_b):
+        Dq = D @ Q[:, j]
+        bkr = -2.0 * Dq
+
+        def matrix(x):
+            y = A.dot(x) - lam[j] * B.dot(x)
+            t = np.dot(U.T, y)
+            y = y - np.dot(U, t)
+            return y
+
+        matop = scipy.sparse.linalg.LinearOperator((n, n), matvec=matrix)
+
+        t = np.dot(U.T, bkr)
+        bkr = bkr - np.dot(U, t)
+        phi, _ = scipy.sparse.linalg.gmres(matop, bkr, M=preop, rtol=1e-10, atol=1e-15)
+
+        hdot += eta[j] * Q[:, j].T @ (Adot - lam[j] * Bdot) @ phi
+        hdot -= eta[j] * h * (Q[:, j].T @ Bdot @ Q[:, j])
+
+        if Ddot is not None:
+            hdot += eta[j] * Q[:, j].T @ Ddot @ Q[:, j]
+
+    return hdot
+
+
+def deriv_approx_E(
     A,
     B,
     D,
@@ -303,21 +438,11 @@ def deriv_approx(
     rho=1.0,
 ):
     """
-    Approximately compute the forward derivative
-
-        first term:
-            sum^N E_{ij} (Dq_{ij} + h delta_{ij})*(Aq_dot_{ij} - lam_{i} Bq_dot_{ij})
-
-        second term:
-            sum^N 2 * q_{i}^T * Adot * v_{j}
-            sum^N q_{i}^T * Bdot * (u_{j} - lam_{j} * v_{j} - w_{j})
-
+    Eignvector Aggregation I
     """
     # normalize the eta vector
     trace = np.sum(eta)
     eta = eta / trace
-
-    # compute the h value
     h = fun_h(eta, Q, D)
     hdot = 0.0
 
@@ -337,7 +462,7 @@ def deriv_approx(
                 scale = qDq - h
             else:
                 scale = qDq
-                
+
             hdot += Eij * scale * (qAdotq - lam[j] * qBdotq)
 
     C = B @ Q[:, N_a:N_b]
@@ -371,109 +496,6 @@ def deriv_approx(
     return hdot
 
 
-def deriv_approx2(
-    A,
-    B,
-    D,
-    Adot,
-    Bdot,
-    lam,
-    Q,
-    eta,
-    fun,
-    Eij_fun,
-    Gij_fun,
-    N_a=0,
-    N_b=5,
-    lam_a=None,
-    lam_b=None,
-    Ddot=None,
-    rho=1.0,
-):
-    # normalize the eta vector
-    trace = np.sum(eta)
-    eta = eta / trace
-
-    # compute the h value
-    h = fun_h(eta, Q, D)
-    hdot = 0.0
-
-    for i in range(N_a, N_b):
-        for j in range(i+1):
-            qDq = Q[:, i].T @ D @ Q[:, j]
-            qAdotq = Q[:, i].T @ Adot @ Q[:, j]
-            qBdotq = Q[:, i].T @ Bdot @ Q[:, j]
-
-            if Eij_fun == Eij_a:
-                Eij = Eij_fun(fun, rho, trace, np.min(lam), lam[i], lam[j])
-                Gij = Gij_fun(fun, rho, trace, np.min(lam), lam[i], lam[j])
-            else:
-                Eij = Eij_fun(fun, rho, trace, lam[i], lam[j], lam_a, lam_b)
-                Gij = Gij_fun(fun, rho, trace, lam[i], lam[j], lam_a, lam_b)
-            
-            # ic(i, j, Eij, Gij)
-            
-
-            if i == j:
-                scale = qDq - h
-            else:
-                scale = 2*qDq
-
-            hdot += scale * (Eij * qAdotq - Gij * qBdotq)
-
-    # compute the orthogonal projector
-    C = B @ Q[:, :N_b]
-    U, _ = np.linalg.qr(C)
-
-    factor = 0.99
-    P = A - factor * lam[0] * B
-    Pfactor = scipy.sparse.linalg.factorized(P)
-
-    def preconditioner(x):
-        y = Pfactor(x)
-        t = np.dot(U.T, y)
-        y = y - np.dot(U, t)
-        return y
-
-    preop = scipy.sparse.linalg.LinearOperator((n, n), matvec=preconditioner)
-
-    Z = np.eye(np.shape(A)[0]) - U @ U.T
-
-    inintial_guess = np.zeros(n)
-    
-    for j in range(np.max([0, N_a - 1]), N_b):
-        Dq = D @ Q[:, j]
-        bkr = -2.0 * Dq
-
-        def matrix(x):
-            y = A.dot(x) - lam[j] * B.dot(x)
-            t = np.dot(U.T, y)
-            y = y - np.dot(U, t)
-            return y
-
-        matop = scipy.sparse.linalg.LinearOperator((n, n), matvec=matrix)
-
-        t = np.dot(U.T, bkr)
-        bkr = bkr - np.dot(U, t)
-        phi, _ = scipy.sparse.linalg.gmres(matop, bkr, x0=inintial_guess, M=preop, tol=1e-10, atol=1e-15)
-        phi = phi * eta[j]
-        inintial_guess = phi - np.dot(U, np.dot(U.T, phi))
-
-        # Ak = A - lam[j] * B
-        # Abar = Z.T @ Ak @ Z
-        # bbar = Z.T @ (-2.0 * eta[j] * Dq)
-        # phi = Z @ np.linalg.solve(Abar, bbar)
-
-        hdot += Q[:, j].T @ (Adot - lam[j] * Bdot) @ phi
-
-        hdot -= eta[j] * (Q[:, j].T @ D @ Q[:, j]) * (Q[:, j].T @ Bdot @ Q[:, j])
-
-        if Ddot is not None:
-            hdot += eta[j] * Q[:, j].T @ Ddot @ Q[:, j]
-
-    return hdot
-
-
 def store_EG(fun, Gr, Kr, ks_rho, N_a, N_b):
     n = 100
 
@@ -490,12 +512,8 @@ def store_EG(fun, Gr, Kr, ks_rho, N_a, N_b):
 
     for i in range(n):
         for j in range(n):
-            E[i, j] = Eij_ab(
-                fun, ks_rho, eta_sum, mu[i], mu[j], lam_a, lam_b
-            )
-            G[i, j] = Gij_ab(
-                fun, ks_rho, eta_sum, mu[i], mu[j], lam_a, lam_b
-            )
+            E[i, j] = Eij_ab(fun, ks_rho, eta_sum, mu[i], mu[j], lam_a, lam_b)
+            G[i, j] = Fij_ab(fun, ks_rho, eta_sum, mu[i], mu[j], lam_a, lam_b)
 
     np.save("data/E.npy", E)
     np.save("data/G.npy", G)
@@ -503,19 +521,19 @@ def store_EG(fun, Gr, Kr, ks_rho, N_a, N_b):
 
 if __name__ == "__main__":
     # Set parameters
-    rho = 10000.0  # how large rho is depends on how close the eigenvalues are
+    rho = 1000.0  # how large rho is depends on how close the eigenvalues are
     n = 100
-    dh = 1e-6
+    dh = 1e-8
+    dhh = 1e-50
     ndvs = 1
 
     np.random.seed(0)
-    # x = np.random.uniform(size=ndvs)
-    # p = np.random.uniform(size=ndvs)
 
     A = rand_symm_mat(n)
-    B = rand_symm_mat(n)  #
-    # B = np.eye(n)
+    # B = rand_symm_mat(n)
+    B = np.eye(n)
     D = rand_symm_mat(n)
+
     Adot = rand_symm_mat(n)
     Bdot = rand_symm_mat(n)
     Ddot = rand_symm_mat(n)
@@ -529,21 +547,15 @@ if __name__ == "__main__":
     B2 = B - dh * Bdot
     D2 = D - dh * Ddot
 
-    # use complex step to check the derivative
-    Acs = A + 1j * dh * Adot
-    Bcs = B + 1j * dh * Bdot
-    Dcs = D + 1j * dh * Ddot
-
     lam, Q = eigh(A, B)
     lam1, Q1 = eigh(A1, B1)
     lam2, Q2 = eigh(A2, B2)
-    lamcs, Qcs = eigh(Acs, Bcs)
 
     # ["exp", "sech", "tanh", "erf", "erfc", "sigmoid", "ncdf"]:
-    for softmax in ["tanh"]:
+    for softmax in ["tanh", "exp"]:
         print("Softmax =", softmax)
 
-        if softmax in ["sech"]:
+        if softmax in ["exp"]:
             fun = getattr(mp, softmax)
 
             N = 10
@@ -555,64 +567,41 @@ if __name__ == "__main__":
             eta = softmax_a(fun, rho, lam)
             eta1 = softmax_a(fun, rho, lam1)
             eta2 = softmax_a(fun, rho, lam2)
-            etacs = softmax_a(fun, rho, lamcs)
 
             Eij_fun = Eij_a
-            Gij_fun = Gij_a
+            Fij_fun = Fij_a
 
-            # use complex step to compute the derivative
             h1 = fun_h(eta1, Q1, D1)
             h2 = fun_h(eta2, Q2, D2)
-            # ic(h1, h2)
 
             ans_cs = (h1 - h2) / (2 * dh)
-            ans_cs = fun_h(etacs, Qcs, Dcs, n).imag / dh
 
         else:
             fun = getattr(mp, softmax)
 
-            a = lam[0] + 10.0
-            b = lam[0] + 50.0
-            lam_a, lam_b, N_a, N_b = compute_lam_value(lam, a, b)
-            lam_a, lam_b, N_a, N_b = compute_lam_index(lam, 3, 6)
+            # compute the lam_a and lam_b based on the value
+            # a = lam[0] + 10.0
+            # b = lam[0] + 50.0
+            # lam_a, lam_b, N_a, N_b = compute_lam_value(lam, a, b)
 
-            if fun == mp.exp:
-                rho *= -1.0
-                eta = -softmax_ab(fun, rho, lam, lam_a, lam_b)
-                eta1 = -softmax_ab(fun, rho, lam1, lam_a, lam_b)
-                eta2 = -softmax_ab(fun, rho, lam2, lam_a, lam_b)
-                etacs = -softmax_ab(fun, rho, lamcs, lam_a, lam_b)
+            # compute the lam_a and lam_b based on the index
+            lam_a, lam_b, N_a, N_b = compute_lam_index(lam, 0, 6)
 
-                h1 = fun_h(eta1, Q1, D1)
-                h2 = fun_h(eta2, Q2, D2)
+            eta = softmax_ab(fun, rho, lam, lam_a, lam_b)
+            eta1 = softmax_ab(fun, rho, lam1, lam_a, lam_b)
+            eta2 = softmax_ab(fun, rho, lam2, lam_a, lam_b)
 
-                ans_cs = (h1 - h2) / (2 * dh)
-                # ans_cs = fun_h(etacs, Qcs, Dcs, n).imag / dh
-            else:
-                eta = softmax_ab(fun, rho, lam, lam_a, lam_b)
-                eta1 = softmax_ab(fun, rho, lam1, lam_a, lam_b)
-                eta2 = softmax_ab(fun, rho, lam2, lam_a, lam_b)
-                etacs = softmax_ab(fun, rho, lamcs, lam_a, lam_b)
+            h1 = fun_h(eta1, Q1, D1)
+            h2 = fun_h(eta2, Q2, D2)
 
-                h1 = fun_h(eta1, Q1, D1)
-                h2 = fun_h(eta2, Q2, D2)
-
-                ans_cs = (h1 - h2) / (2 * dh)
-                # ans_cs = fun_h(etacs, Qcs, Dcs, n).imag / dh
-
-            ic(fun)
-            ic(rho)
-            ic(lam_a, lam_b, N_a, N_b)
-            ic(lam[:10])
-            ic(eta[:10])
+            ans_cs = (h1 - h2) / (2 * dh)
 
             Eij_fun = Eij_ab
-            Gij_fun = Gij_ab
+            Fij_fun = Fij_ab
 
-        ans = deriv_exict(
+        ans_exict = deriv_exict_EF(
             rho,
             A,
-            B,
             D,
             Adot,
             Bdot,
@@ -621,13 +610,13 @@ if __name__ == "__main__":
             eta,
             fun,
             Eij_fun,
-            Gij_fun,
+            Fij_fun,
             lam_a,
             lam_b,
             Ddot,
         )
 
-        ans_approx = deriv_approx(
+        ans_approx = deriv_approx_EF(
             A,
             B,
             D,
@@ -638,6 +627,7 @@ if __name__ == "__main__":
             eta,
             fun,
             Eij_fun,
+            Fij_fun,
             N_a,
             N_b,
             lam_a,
@@ -646,62 +636,9 @@ if __name__ == "__main__":
             rho=rho,
         )
 
-        ans_approx2 = deriv_approx2(
-            A,
-            B,
-            D,
-            Adot,
-            Bdot,
-            lam,
-            Q,
-            eta,
-            fun,
-            Eij_fun,
-            Gij_fun,
-            N_a,
-            N_b,
-            lam_a,
-            lam_b,
-            Ddot,
-            rho=rho,
-        )
-
-        # if softmax in ["sech"]:
-        print("ans", ans)
+        print("ans_exict", ans_exict)
         print("ans_cs =", ans_cs)
-        print("error_cs_1 =", np.abs(ans_cs - ans_approx) / np.abs(ans_cs))
-        print("error_cs_2 =", np.abs(ans_cs - ans_approx2) / np.abs(ans_cs))
-
-        print("ans = ", ans)
-        print("ans_approx1 = ", ans_approx)
-        print("ans_approx2 = ", ans_approx2)
-        print("error1  =", np.abs(ans - ans_approx) / np.abs(ans))
-        print("error2  =", np.abs(ans - ans_approx2) / np.abs(ans))
-        print("")
-
-    #     plt.plot(lam, eta / np.sum(eta), "o-", label=softmax)
-    #     plt.legend()
-    # plt.show()
-    
-        # lam = np.array([-0.03699669, -0.02408163, -0.02408163, -0.02373252, -0.02215617,
-        #              -0.01048916, -0.01034584, -0.00960726, -0.009055806, -0.00904732])
-        # a = 1
-        # b = 4
-        # rho = 10000.0
-        # lam_a, lam_b, N_a, N_b = compute_lam_index(lam, a, b)
-        # ic(lam_a, lam_b, N_a, N_b)
-        # eta = softmax_ab(fun, rho, lam, lam_a, lam_b)
-        # eta_sum = np.sum(eta)
-        # ic(eta)
-        # ic(eta / np.sum(eta))
-        
-        # for i in range(len(lam)):
-        #     for j in range(i+1):
-        #         # ic(fun, rho, eta_sum, lam[i], lam[j], lam_a, lam_b)
-        #         Eij = Eij_ab(fun, rho, eta_sum, lam[i], lam[j], lam_a, lam_b)
-        #         Gij = Gij_ab(fun, rho, eta_sum, lam[i], lam[j], lam_a, lam_b)
-        #         ic(i, j, Eij, Gij)
-                
-        # plt.plot(lam, eta / np.sum(eta), "o-", label=softmax)
-        # plt.legend()
-        # plt.show()
+        print("cs_exict =", np.abs(ans_exict - ans_cs) / np.abs(ans_cs))
+        print(
+            "exict_approx =", np.abs(ans_exict - ans_approx) / np.abs(ans_exict), "\n"
+        )
